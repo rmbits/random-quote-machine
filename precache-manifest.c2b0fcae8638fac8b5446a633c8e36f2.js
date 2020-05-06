2d9e70f2c9bce11ce98bef7df305f69d95e1543b
rmbits <rmwebstack@gmail.com>
2020-05-06 17:17:05 -0400

    Updates

---
+++ precache-manifest.c2b0fcae8638fac8b5446a633c8e36f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acaf97216ac532ed63d6fa9aca6c2cf9",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "902c2b0521a9bfde8037",
+    "url": "/random-quote-machine/static/css/main.568a2ba4.chunk.css"
+  },
+  {
+    "revision": "b0b56f761cf40b60acef",
+    "url": "/random-quote-machine/static/js/2.1d57432a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.1d57432a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "902c2b0521a9bfde8037",
+    "url": "/random-quote-machine/static/js/main.f5d18a08.chunk.js"
+  },
+  {
+    "revision": "169f538802cee213882f",
+    "url": "/random-quote-machine/static/js/runtime-main.4d3b854a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random-quote-machine/static/media/logo.5d5d9eef.svg"
+  }
+]);
