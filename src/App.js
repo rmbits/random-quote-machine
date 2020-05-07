fee3784dcb4ef0e6e22cba96068db0c5b003d23e
rmbits <rmwebstack@gmail.com>
2020-05-07 16:38:35 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './quotes.json'
 
-function App() {
-  return (
-    <div id="quote-box" className="">
-      <div id="text">text
+//const API = 'https://api.jsonbin.io/b/5eb422ae8284f36af7b71ab7/2';
+//const authors = data.map((item, index) => <li key={index}>{item.Quote}</li>);
 
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      randomIndex: 1
+    };
+    
+    this.getNewQuote = this.getNewQuote.bind(this);
+  }
+
+  getNewQuote() {
+    this.setState({
+      randomIndex: Math.floor(Math.random() * data.length)
+    });
+  }
+
+  render() {
+
+    const quote = data[this.state.randomIndex].Quote;
+    const author = data[this.state.randomIndex].Author;
+    const twitterUrl = encodeURI('https://twitter.com/intent/tweet?hashtags=quotes&text="' + quote + '" - ' + author);
+
+    return (
+      <div className="wrapper">
+        <div id="quote-box" className="quote-box">
+          
+          <div className="quote">
+            <div id="text">{quote}</div>
+            <div id="author"> - {author}</div>
+          </div>
+
+          <i className="fa fa-twitter"></i>
+          <a href={twitterUrl} id="tweet-quote" className="twitterLink" target="_blank" title="Tweet it!" >Tweet</a>
+          <button id="new-quote" className="btn btn-primary" onClick={this.getNewQuote}>New Quote</button>          
+          
+        </div>
       </div>
-      
-      <div id="author">author
-
-      </div>
-      
-      <button id="new-quote" className="btn btn-dark">New Quote</button>
-
-      <a href="#" id="tweet-quote">Tweet</a>
-
-
-
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
